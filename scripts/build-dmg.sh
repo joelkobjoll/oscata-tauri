2238ef3f55f985882410301d5faa9e88ae52c37c
Joel Kobjoll <kobjoll@gmail.com>
2026-04-03 19:42:15 +0200

    feat: ensure nvm is installed before building DMG package

--- scripts/build-dmg.sh
+++ scripts/build-dmg.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 
+if [ ! -f "$HOME/.nvm/nvm.sh" ]; then
+  echo "Installing nvm..."
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+fi
+
 source ~/.nvm/nvm.sh
+nvm install 22 --no-progress
 nvm use 22
 
 cd "$(dirname "$0")/.."
